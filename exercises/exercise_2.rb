41f675d60b4ce88ea929ccbf0d6642bfd5e431de
Olivia <oliviapower.02@gmail.com>
2020-03-02 23:19:19 +0000

    complete exercise 3 and 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,10 +1,6 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
-# 2. Load the second store from the database and assign it to `@store2`.
-# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
-
 puts "Exercise 2"
 puts "----------"
 
@@ -14,5 +10,3 @@ puts "----------"
 @store2 = Store.find_by(id: 1)
 @store1.name = 'Metro'
 @store1.save
-
-puts @store1.name
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,6 +4,12 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
+### Exercise 5: Calculations
+
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
 puts "Exercise 5"
 puts "----------"
 
